64ede8849fcb294f1c52faa1e0f25684a25b0ae7
Roberto DeDeus Jr <roberto.p.dedeus@gmail.com>
2019-09-19 17:02:53 -0400

    Add email settings, contact forms, and tests.

--- booktime/settings.py
+++ booktime/settings.py
@@ -119,3 +119,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+if not DEBUG:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_HOST_USER = 'username'
+    EMAIL_HOST = 'smtp.domain.com'
+    EMAIL_PORT = 587
+    EMAIL_USE_TLS = True
+    EMAIL_HOST_PASSWORD = 'password'
+else:
+    EMAIL_BACKEND = (
+        "django.core.mail.backends.console.EmailBackend"
+    )
+
