18d5b3aa47d3e3bdffef420907957d0b5b77408a
rpdedeus <roberto.p.dedeus@gmail.com>
2019-09-20 17:17:13 -0400

    Implement custom model admins.

--- main/models.py
+++ main/models.py
@@ -19,6 +19,9 @@ class Product(models.Model):
 
     objects = ActiveManager()
 
+    def __str__(self):
+        return self.name
+
 
 class ProductImage(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
