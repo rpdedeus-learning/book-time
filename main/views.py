62c41caec4a247b87858bc499254c742fc8009fc
rpdedeus <roberto.p.dedeus@gmail.com>
2019-09-20 21:31:45 -0400

    Add product list view.

--- main/views.py
+++ main/views.py
@@ -1,7 +1,29 @@
 from django.views.generic.edit import FormView
-from main import forms
+from main import forms, models
+from django.views.generic.list import ListView
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
+
+
+class ProductListView(ListView):
+
+    template_name = "main/product_list.html"
+    paginate_by = 4
+
+    def get_queryset(self):
+        tag = self.kwargs['tag']
+        self.tag = None
+
+        if tag != "all":
+            self.tag = get_object_or_404(models.ProductTag, slug=tag)
+
+        if self.tag:
+            products = models.Product.objects.active().filter(tags=self.tag)
+        else:
+            products = models.Product.objects.active()
+
+        return products.order_by("name")
 
 
 class ContactUsView(FormView):
