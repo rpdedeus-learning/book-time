b24f1de8f0ee737594e2cef11eb37b52fbe030c8
Roberto DeDeus Jr <roberto.p.dedeus@gmail.com>
2019-09-23 12:00:40 -0400

    Re-arrange urls.py

--- main/urls.py
+++ main/urls.py
@@ -3,9 +3,9 @@ from django.views.generic import TemplateView, DetailView
 from main import views, models
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name="home.html"), name="home"),
+    path('about-us/', TemplateView.as_view(template_name="about_us.html"), name="about_us"),
+    path('contact-us/', views.ContactUsView.as_view(), name="contact_us"),
     path('product/<slug:slug>/', DetailView.as_view(model=models.Product), name='product'),
     path('products/<slug:tag>/', views.ProductListView.as_view(), name="products"),
-    path('contact-us/', views.ContactUsView.as_view(), name="contact_us"),
-    path('about-us/', TemplateView.as_view(template_name="about_us.html"), name="about_us"),
-    path('', TemplateView.as_view(template_name="home.html"), name="home"),
 ]
