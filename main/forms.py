a105d865800ad2ac200c175fa6e3c43289afa474
Roberto DeDeus Jr <roberto.p.dedeus@gmail.com>
2019-09-23 15:23:56 -0400

    Implement login feature.

--- main/forms.py
+++ main/forms.py
@@ -1,6 +1,7 @@
 import logging
 
 from django import forms
+from django.contrib.auth import authenticate
 from django.contrib.auth.forms import (
     UserCreationForm as DjangoUserCreationForm
 )
@@ -10,6 +11,31 @@ from django.core.mail import send_mail
 from . import models
 
 logger = logging.getLogger(__name__)
+
+
+class AuthenticationForm(forms.Form):
+
+    email = forms.EmailField()
+    password = forms.CharField(strip=False, widget=forms.PasswordInput)
+
+    def __init__(self, request=None, *args, **kwargs):
+        self.request = request
+        self.user = None
+        super().__init__(*args, **kwargs)
+
+    def clean(self):
+        email = self.cleaned_data.get("email")
+        password = self.cleaned_data.get("password")
+
+        if email is not None and password:
+            self.user = authenticate(self.request, email=email, password=password)
+            if self.user is None:
+                raise forms.ValidationError("Invalid email/password combination")
+            logger.info("Authentication successful for email=%s", email)
+            return self.cleaned_data
+
+    def get_user(self):
+        return self.user
 
 
 class UserCreationForm(DjangoUserCreationForm):
--- main/urls.py
+++ main/urls.py
@@ -1,12 +1,22 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 from django.views.generic import TemplateView, DetailView
-from main import views, models
+
+from main import views, models, forms
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name="home.html"), name="home"),
     path('about-us/', TemplateView.as_view(template_name="about_us.html"), name="about_us"),
     path('contact-us/', views.ContactUsView.as_view(), name="contact_us"),
     path('signup/', views.SignupView.as_view(), name="signup"),
+    path(
+        'login/',
+        auth_views.LoginView.as_view(
+            template_name="login.html",
+            form_class=forms.AuthenticationForm,
+        ),
+        name="login",
+    ),
     path('product/<slug:slug>/', DetailView.as_view(model=models.Product), name='product'),
     path('products/<slug:tag>/', views.ProductListView.as_view(), name="products"),
 ]
